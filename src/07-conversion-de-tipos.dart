ef3aa5b351aaa4b96fb20f3caeaa85612e995e35
jhondevcode <jhondev.code@gmail.com>
2022-05-22 16:23:24 -0500

    Conversion de tipos

---
+++ src/07-conversion-de-tipos.dart
@@ -0,0 +1,10 @@
+void main() {
+  String texto = "12.5";
+  double numero = double.parse(texto);
+  //int numero = int.parse(texto);
+  print(numero);
+
+  int entero = 24;
+  texto = entero.toString();
+  print(texto);
+}
